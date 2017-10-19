f07742ba6de2333c7ff065540f13da369c7e7e08
ahoink <ahoink@users.noreply.github.com>
2017-10-19 09:35:59 -0400

    Added comments

--- Agent.py
+++ Agent.py
@@ -25,13 +25,15 @@ class Agent():
 			temp1, = self.axis.plot(self.eyes[i][:2], self.eyes[i][2:], color='black')
 			self.eyesPlot.append(temp1)
 
-		self.epsilon = 0.1
-		self.alpha = 0.2
-		self.gamma = 0.9
+		self.epsilon = 0.1	# exploration rate [0, 1] (higher means more random actions)
+		self.alpha = 0.2	# learning rate (0, 1] (higher means it forgets old info quicker)
+		self.gamma = 0.9	# Greediness [0,1] (lower means cares more about immediate rewards)
 		self.q = {}
 		self.actions = [0, 1, 2, 3, 4] #forward, turn left little, turn right little, turn left more, turn right more
 	
 	def move(self, dist):
+	# Move agent in direction of center eye by specified distance
+
 		distx = -math.sin(self.angle - math.pi / 2) * dist
 		disty = math.cos(self.angle - math.pi / 2) * dist
 		self.center = self.center[0] + distx, self.center[1] + disty
@@ -43,6 +45,8 @@ class Agent():
 			self.eyesPlot[i].set_ydata(self.eyes[i][2:])
 
 	def turn(self, angle):
+	# Turn agent in place by specified angle
+
 		angle = angle * math.pi / 180
 		for i in range(self.numEyes):
 			newx = (self.eyes[i][1] - self.eyes[i][0]) * math.cos(angle) - (self.eyes[i][3] - self.eyes[i][2]) * math.sin(angle) + self.eyes[i][0]
@@ -53,6 +57,8 @@ class Agent():
 		self.angle = (self.angle + angle) % (2 * math.pi)
 
 	def atEdge(self):
+	# Check if any eyes see far enough beyond and edge that the agent is at the edge
+
 		for i in range(self.numEyes):
 			if not (-self.viewDist + self.circle.radius * 2 < self.eyes[i][1] < self.axis.get_xlim()[1] + self.viewDist - self.circle.radius * 2 ) or \
 			not (-self.viewDist + self.circle.radius * 2 < self.eyes[i][3] < self.axis.get_ylim()[1] + self.viewDist - self.circle.radius * 2):
@@ -60,6 +66,8 @@ class Agent():
 		return False
 
 	def nearEdge(self):
+	# Check if any eyes see any distance beyond an edge
+
 		for i in range(self.numEyes):
 			if not (0 < self.eyes[i][1] < self.axis.get_xlim()[1]) or not (-0 < self.eyes[i][3] < self.axis.get_ylim()[1]):
 				return True
@@ -69,6 +77,8 @@ class Agent():
 		return self.q.get((state, action), 0.0)
 
 	def learnQ(self, state, action, reward, value):
+	# Set reward for state/action or update existing reward
+
 		oldv = self.q.get((state, action), None)
 		if oldv is None:
 			self.q[(state, action)] = reward
@@ -76,6 +86,9 @@ class Agent():
 			self.q[(state, action)] = oldv + self.alpha * (value - oldv)
 
 	def chooseAction(self, state):
+	# Choose random action based on exploration rate (epsilon) 
+	# or choose best action based on potential rewards
+
 		if random.random() < self.epsilon:
 			action = random.choice(self.actions)
 		else:
@@ -92,6 +105,8 @@ class Agent():
 		return action
 
 	def learn(self, state1, action1, reward, state2):
+	# Learn based on looking in the future for potential rewards
+
 		maxqnew = max([self.getQ(state2, a) for a in self.actions])
 		self.learnQ(state1, action1, reward, reward + self.gamma * maxqnew)
 
@@ -102,5 +117,3 @@ class Agent():
 	def loadQ(self):
 		with open('model.pkl', 'rb') as f:
 			self.q = pickle.load(f)
-
-	
